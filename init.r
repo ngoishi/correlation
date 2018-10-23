e5b9876d9807fa5568ddb63813293375d615f397
ngoishi <43993679+ngoishi@users.noreply.github.com>
2018-10-23 21:55:20 +0900

    Add files via upload

---
+++ init.r
@@ -0,0 +1,10 @@
+#
+# Example R code to install packages
+# See http://cran.r-project.org/doc/manuals/R-admin.html#Installing-packages for details
+#
+
+###########################################################
+# Update this line with the R packages to install:
+
+install.packages("plyr",dependencies=TRUE)
+
